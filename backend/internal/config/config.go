38b6b2f68f24d8051f5b3b79d3064d53c0853948
Pav <laurapav1@hotmail.com>
2025-07-24 23:00:34 +0200

    added docker compose and it's working

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,9 +1,18 @@
 package config
 
+import "os"
+
 type Config struct {
-  Port, DBHost, DBPort, DBUser, DBPass, DBName string
+	Port, DBHost, DBPort, DBUser, DBPass, DBName string
 }
 
 func Load() *Config {
-  return &Config{}
+	return &Config{
+		Port:   os.Getenv("PORT"),
+		DBHost: os.Getenv("DB_HOST"),
+		DBPort: os.Getenv("DB_PORT"),
+		DBUser: os.Getenv("DB_USER"),
+		DBPass: os.Getenv("DB_PASS"),
+		DBName: os.Getenv("DB_NAME"),
+	}
 }
